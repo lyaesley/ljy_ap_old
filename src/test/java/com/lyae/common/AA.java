8d92585bbce07a26d4a671620cf04c5db3b93fd5
이준영 <lee-jy@partner.sk.com>
2017-01-13 18:00:31 +0900

    Test Calendar

--- src/test/java/com/lyae/common/AA.java
+++ src/test/java/com/lyae/common/AA.java
@@ -1,5 +1,21 @@
 package com.lyae.common;
 
-public class AA {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class AA {
+	
+	public static void main(String args[]){
+		
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		System.out.println(cal.getTime());
+		
+		System.out.println(dateFormat.format(cal.getTime()));
+		cal.add(Calendar.HOUR, 3);
+		System.out.println(dateFormat.format(cal.getTime()));
+		
+	}
+	
+	
 }
