d70a1350b2612833f413698217f7e0ec21680140
이준영 <lee-jy@partner.sk.com>
2017-02-15 11:08:11 +0900

    @scheduled 설정법 주석추가

--- src/test/java/com/lyae/common/CommonTest.java
+++ src/test/java/com/lyae/common/CommonTest.java
@@ -50,7 +50,7 @@ public class CommonTest {
 		System.out.println("44"+o1);
 		o2 = srv.sortHomeAwaybyResult(sortList, o1);
 		System.out.println("55"+o2);
-		//ÇÖÇÈ½º Ãß°¡
+		//ï¿½ï¿½ï¿½È½ï¿½ ï¿½ß°ï¿½
 		System.out.println("66");
 		
 	}
